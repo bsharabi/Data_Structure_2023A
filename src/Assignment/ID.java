cf59a043f3662f0aad65133ce62e8328a34255e5
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-11-08 21:31:16 +0200

    fix assignment ID file + add TestExample class

--- src/Assignment/ID.java
+++ src/Assignment/ID.java
@@ -1,15 +1,21 @@
 package Assignment;
 
+// יש לשנות את שם המחלקה למספר ת"ז של המגיש וכמובן את שם הקובץ גם כן.
 public class ID {
 
     public void q1(int n) {
-
+        int a = 1, b = 1;
+        System.out.println(n + " = " + a + " + " + b);
     }
 
     public void q2(int n) {
+        int a = 1, b = 1, c = 1;
+        System.out.println(n + " = " + a + " + " + b + " + " + c);
     }
 
 
     public void q3(int n) {
+        int a = 1, b = 1, c = 1;
+        System.out.println(n + " = " + a + " + " + b + " + " + c);
     }
 }
