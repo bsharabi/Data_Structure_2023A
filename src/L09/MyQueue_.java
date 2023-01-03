ca973c7a01790efdeb2147d627da460341bec8c1
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2023-01-03 18:25:51 +0200

    new practice l09

--- src/L09/MyQueue_.java
+++ src/L09/MyQueue_.java
@@ -1,16 +1,23 @@
 package L09;
 
+import java.util.Arrays;
+
 public class MyQueue_<E> {
+
     public static final int DEFAULT_CAPACITY = 10;
     private Object[] qArray;
+
     private int size;// count of item in the qArray
     private int capacity;// like qArray.len
+
     private int indexToInsert = 0;
     private int indexToRemove = 0;
+
     public MyQueue_() {
         qArray = new Object[DEFAULT_CAPACITY];
         capacity = DEFAULT_CAPACITY;
     }
+
     public MyQueue_(int initialCapacity) throws Exception {
         if (initialCapacity < 0)
             throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
@@ -22,6 +29,7 @@ public class MyQueue_<E> {
             capacity = initialCapacity;
         }
     }
+
     public boolean add(E o) {
         if (size == capacity)
             return false;
@@ -31,22 +39,33 @@ public class MyQueue_<E> {
         size++;
         return true;
     }
+
     public E poll() throws Exception {
         if (size == 0)
-            return null;
+            throw new Exception("");
         E item = (E) qArray[indexToRemove];
-        indexToRemove = indexToRemove + 1;
-        indexToRemove %= indexToRemove;
+        qArray[indexToRemove]=null;
+        indexToRemove = (indexToRemove + 1)%capacity;
         size--;
         return item;
     }
+
     public boolean isEmpty() {
-        return size==0;
+        return size == 0;
     }
+
     public E peek() throws Exception {
         if (isEmpty())
             return null;
         E item = (E) qArray[indexToRemove];
         return item;
+    }
+
+    public int capacity(){
+        return capacity;
+    }
+    @Override
+    public String toString() {
+        return Arrays.toString(qArray);
     }
 }
